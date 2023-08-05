0c17a552d87a948ba5b0b7c95d4b9ed295619900
Krzysztof J. Czarnecki <kjczarne@gmail.com>
2023-08-05 15:12:44 -0400

    Add `iter_compare`

--- src/unintelligent/utils/iterators.py
+++ src/unintelligent/utils/iterators.py
@@ -1,5 +1,5 @@
 from typing import Iterable, Iterator, Any, List
-from itertools import islice
+from itertools import islice, accumulate
 
 
 # pylint: disable-next=invalid-name
@@ -19,3 +19,19 @@ def take_n(iterable: Iterable[Any], n: int) -> Iterator[List[Any]]:
         if chunk is None:
             break
         yield chunk
+
+
+def iter_compare(actual: Iterable[Any], expected: Iterable[Any]) -> bool:
+    """Compares two iterables element-by element. Optimized with
+    `map` and `accumulate` for relatively fast execution times.
+
+    Args:
+        actual (Iterable[Any]): first iterable to compare (e.g. a list)
+        expected (Iterable[Any]): second iterable to compare (e.g. another list)
+
+    Returns:
+        bool: `True` if the two iterables consist of identical elements, `False` otherwise
+    """
+    return all(accumulate(map(lambda x: x[0] == x[1],  # compare (x[0], x[1]) in the zipped iterables
+                              zip(actual, expected, strict=True)),
+                          lambda x, y: x and y))
